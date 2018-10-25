deed234e432f917a868049fb12935e7fa320749a
Ricardo Marques <ricardooo.marques@gmail.com>
2018-10-26 04:16:07 +0800

    Fix timeapp.go with go fmt

--- timeapp/timeapp.go
+++ timeapp/timeapp.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-  "time"
-  "net/http"
-  "io"
-  "log"
+	"io"
+	"log"
+	"net/http"
+	"time"
 )
 
 func main() {
-  http.HandleFunc("/now", nowTime)
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-    log.Printf("An error ocurred: %s\n", err)
-  }
+	http.HandleFunc("/now", nowTime)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Printf("An error ocurred: %s\n", err)
+	}
 }
 
 func nowTime(w http.ResponseWriter, r *http.Request) {
-  io.WriteString(w, time.Now().UTC().String())
+	io.WriteString(w, time.Now().UTC().String())
 }
